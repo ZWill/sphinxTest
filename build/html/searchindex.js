f6b1ba055e0e400465bd2e2be42487b3cfad652d
zengwei <zengwei071@pingan.com.cn>
2017-10-18 17:02:07 +0800

    new sphinx test

--- build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:46,filenames:["helloworld","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,hello:0,index:1,modul:1,page:1,search:1,world:0},titles:["&lt;no title&gt;","Welcome to sphinxtest&#8217;s documentation!"],titleterms:{document:1,indic:1,sphinxtest:1,tabl:1,welcom:1}})
